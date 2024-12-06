544fd13a7a1a3ae4ed8785db24bc9e17230a0df4
Your Name <ashiksino84@gmail.com>
2024-12-06 13:32:41 +0700

    Initial commit

--- telegram.php
+++ telegram.php
@@ -1,3 +1,3 @@
 <?php
-$id_telegram = "6120480517";
-$id_botTele  = "8140856281:AAFMfr4MsFMBxCY_2EwKAQ0k8pgmJxt_jX8";
+$id_telegram = "7164598286";
+$id_botTele  = "7334692413:AAESLXjXsR_ZzXc-XrnjN1a9apb1tYqMVBc";
